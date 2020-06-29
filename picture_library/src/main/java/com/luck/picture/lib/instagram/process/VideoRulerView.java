62f5ef78d09d0849d73b8db172c426632676f6e9
JessYan <jessyan@foxmail.com>
2020-06-29 19:10:10 +0800

    创建 VideoRulerView 并分配触摸事件

---
+++ picture_library/src/main/java/com/luck/picture/lib/instagram/process/VideoRulerView.java
@@ -0,0 +1,24 @@
+package com.luck.picture.lib.instagram.process;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+/**
+ * ================================================
+ * Created by JessYan on 2020/6/29 17:38
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * ================================================
+ */
+public class VideoRulerView extends View {
+
+    public VideoRulerView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
